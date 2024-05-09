5bdec0ce21a6271133180283f34c3c28336c0d07
Jérôme Peyras <jerome.peyras@protonmail.com>
2024-05-09 17:54:25 +0200

    Adds custom login template as Homepage

--- litrevu/litrevu/urls.py
+++ litrevu/litrevu/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__reload__/', include("django_browser_reload.urls"))
+    path('accounts/', include('authentication.urls')),
+    path('', auth_views.LoginView.as_view()),
+    path('__reload__/', include('django_browser_reload.urls')),
 ]

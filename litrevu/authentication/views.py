33d19afb6ef93b372dc45c331e6c2e363b34590a
Jérôme Peyras <jerome.peyras@protonmail.com>
2024-05-10 11:07:54 +0200

    Custom login page and form

--- litrevu/authentication/views.py
+++ litrevu/authentication/views.py
@@ -1,2 +1,22 @@
+from django import forms
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 
+
+class SignInForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "text-input", "placeholder": "Nom d'utilisateur"}
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "password-input", "placeholder": "Mot de passe"}
+        )
+    )
+
+
+class SignInView(LoginView):
+    form_class = SignInForm
+    template_name = "authentication/signin.html"
--- litrevu/litrevu/urls.py
+++ litrevu/litrevu/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth import views as auth_views
+
+from authentication.views import SignInView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('authentication.urls')),
-    path('', auth_views.LoginView.as_view()),
-    path('__reload__/', include('django_browser_reload.urls')),
+    path("admin/", admin.site.urls),
+    path("", SignInView.as_view(), name="signin"),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
